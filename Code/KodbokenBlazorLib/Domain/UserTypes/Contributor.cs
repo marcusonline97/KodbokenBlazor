0361e9f9f3f9c6b7d93a66b193df9aad1ecd6d07
marcusonline97 <Mjpersson97@gmail.com>
2024-02-18 17:23:33 +0100

    Added some preset information

---
+++ Code/KodbokenBlazorLib/Domain/UserTypes/Contributor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodbokenBlazorLib.Contributors
+{
+    public class Contributor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string ProgramLang { get; set; } = string.Empty;
+
+        public bool HasProfilePic { get; set; } = default!;
+
+    }
+}
